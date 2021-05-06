8e7db48ac9d45f1e2f4c25e2a081032024cdd6ab
Amanda <amanda.garrison.woods@gmail.com>
2021-05-06 16:58:34 -0500

    making a spooky ghost with writeline

---
+++ ghost.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      //creating a ghost with write line
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("This is my spooky ghost Charles");
+     }
+  }
+}
